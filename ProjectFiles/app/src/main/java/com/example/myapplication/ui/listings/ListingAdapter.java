20c3e6267178d618112bc345b6d73129e7e06f89
spncrf <spncrf@gmail.com>
2020-02-21 10:25:04 -0600

    Building the Show Listings view

---
+++ ProjectFiles/app/src/main/java/com/example/myapplication/ui/listings/ListingAdapter.java
@@ -0,0 +1,76 @@
+package com.example.myapplication.ui.listings;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingViewHolder> {
+    private ArrayList<ShowListing> showListings;
+
+    public static class ListingViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextBandName;
+        public TextView mTextVenue;
+        public TextView mTextTime;
+        public TextView mInterestedText;
+        public ImageView mBandFavorited;
+        public ImageView mVenueFavorited;
+        public CheckBox mUserInterested;
+
+        public ListingViewHolder(View itemView){
+            super(itemView);
+            mTextBandName = itemView.findViewById(R.id.bandName);
+            mTextVenue = itemView.findViewById(R.id.venue);
+            mTextTime = itemView.findViewById(R.id.time);
+            mInterestedText = itemView.findViewById(R.id.interested_text);
+            mBandFavorited = itemView.findViewById(R.id.bandFavorited);
+            mVenueFavorited = itemView.findViewById(R.id.venueFavorited);
+            mUserInterested = itemView.findViewById(R.id.interested);
+        }
+    }
+
+    public ListingAdapter(ArrayList<ShowListing> showList) {
+        this.showListings = showList;
+    }
+
+    @NonNull
+    @Override
+    public ListingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.show_listing, parent, false);
+        ListingViewHolder evh = new ListingViewHolder(v);
+        return evh;
+    }
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+    @Override
+    public void onBindViewHolder(final @NonNull ListingViewHolder holder, int position) {
+        final ShowListing currentItem = this.showListings.get(position);
+        holder.mTextBandName.setText(currentItem.getBandName());
+        holder.mTextVenue.setText(currentItem.getVenue());
+        holder.mTextTime.setText(currentItem.getTime());
+        holder.mInterestedText.setText(currentItem.getNumberInterested());
+        if (currentItem.getBandFavorite()){
+            holder.mBandFavorited.setImageResource(R.drawable.favorites_icon_24dp);
+        }
+        if (currentItem.getVenueFavorite()){
+            holder.mVenueFavorited.setImageResource(R.drawable.favorites_icon_24dp);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.showListings.size();
+    }
+}
--- ProjectFiles/app/src/main/java/com/example/myapplication/ui/listings/ListingsFragment.java
+++ ProjectFiles/app/src/main/java/com/example/myapplication/ui/listings/ListingsFragment.java
@@ -11,8 +11,12 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
+
+import java.util.ArrayList;
 
 public class ListingsFragment extends Fragment {
 
@@ -20,16 +24,38 @@ public class ListingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView allListings;
+        RecyclerView.Adapter mAdapter;
+        RecyclerView.LayoutManager mLayoutManager;
+
         listingsViewModel =
                 ViewModelProviders.of(this).get(ListingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_listings, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        listingsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        ArrayList<ShowListing> showLists = new ArrayList<>();
+        showLists.add(new ShowListing("Banana Rays", "The Alcove", "6:00pm"));
+        showLists.add(new ShowListing("Gears", "Rounders", "6:30pm"));
+        showLists.add(new ShowListing("The Beatles", "High Tide", "7:00pm"));
+        showLists.add(new ShowListing("Daft Punk", "House Show", "7:30pm"));
+        showLists.add(new ShowListing("Billie Eilish", "The Grammies", "8:00pm"));
+        showLists.add(new ShowListing("Justin Bieber", "The Ferguson Center", "8:30pm"));
+        showLists.add(new ShowListing("Banana Rays", "The Alcove", "9:00pm"));
+        showLists.add(new ShowListing("Foo Fighters", "Mellow Mushroom", "9:30pm"));
+        showLists.add(new ShowListing("Ariana Grande", "Sigma Chi", "10:00pm"));
+        showLists.add(new ShowListing("Jack Stauber", "Red Shed", "10:30pm"));
+        showLists.add(new ShowListing("Taylor Swift", "Houndstooth", "11:00pm"));
+        showLists.add(new ShowListing("Lil' Nas X", "Rhythm and Brews", "11:30pm"));
+        showLists.add(new ShowListing("Drake", "Green Bar", "12:00am"));
+        mAdapter = new ListingAdapter(showLists);
+
+
+;
+
+        allListings = root.findViewById(R.id.listings);
+        allListings.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this.getContext());
+        allListings.setLayoutManager(mLayoutManager);
+        allListings.setAdapter(mAdapter);
         return root;
     }
+
 }
