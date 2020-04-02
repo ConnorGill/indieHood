60215e43b8fbeb0edb831f27fdfb2cb5b6a4b4f6
Connor Gill <connordgill@gmail.com>
2020-04-02 13:06:51 -0500

    db update

--- ProjectFiles/app/src/main/java/com/indiehood/app/ui/report/ReportFragment.java
+++ ProjectFiles/app/src/main/java/com/indiehood/app/ui/report/ReportFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -12,11 +15,20 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.indiehood.app.R;
+
+import java.util.HashMap;
 
 public class ReportFragment extends Fragment {
 
     private ReportViewModel reportViewModel;
+
+    //private Button mReportButton;
+    //private EditText mReportText;
+
+    //private FirebaseFirestore mFireStore;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +42,32 @@ public class ReportFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        //mFireStore = FirebaseFirestore.getInstance();
+
+        //mReportText = (EditText) getView().findViewById(R.id.reportText);
+        //mReportButton = (Button) getView().findViewById(R.id.reportButton);
+
+        /*mReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userReport = mReportText.getText().toString();
+
+                Map<String, String> userMap = new HashMap<>();
+
+                userMap.put( k: "inputText", userReport);
+
+                mFireStore.collection(s: "UserInputCol").add(userMap).addOnSuccessListener< DocumentReference() >
+                        @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                    Toast.makeText(ReportFragment.this, "Report has been sent!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }); */
+
+
         return root;
     }
+
+
 }
--- ProjectFiles/app/src/main/java/com/indiehood/app/ui/suggest/SuggestFragment.java
+++ ProjectFiles/app/src/main/java/com/indiehood/app/ui/suggest/SuggestFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,11 +14,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.indiehood.app.R;
 
 public class SuggestFragment extends Fragment {
 
     private SuggestViewModel suggestViewModel;
+
+    /*private Button mVenueButton;
+    private EditText mVenueName;
+    private EditText mVenueAdd;
+    private EditText mVenueNum;
+    private EditText mVenueText;
+
+    private FirebaseFirestore mFireStore; */
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +42,16 @@ public class SuggestFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        /*mFireStore = FirebaseFirestore.getInstance();
+
+        mVenueName = (EditText) getView().findViewById(R.id.venueName);
+        mVenueAdd = (EditText) getView().findViewById(R.id.venueAdd);
+        mVenueNum = (EditText) getView().findViewById(R.id.venueNum);
+        mVenueText = (EditText) getView().findViewById(R.id.venueText);
+        mVenueButton = (Button) getView().findViewById(R.id.venueButton);*/
+
+
         return root;
     }
 }
