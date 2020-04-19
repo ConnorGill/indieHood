e2f039607a44f36caf86ea5b9dff957112a0b9fa
Lexi Elston <48029301+alelston1@users.noreply.github.com>
2020-04-19 10:46:02 -0500

    UI For Registration Page

--- ProjectFiles/app/src/main/java/com/indiehood/app/ui/register/RegisterFragment.java
+++ ProjectFiles/app/src/main/java/com/indiehood/app/ui/register/RegisterFragment.java
@@ -6,14 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.indiehood.app.R;
+
+import org.w3c.dom.Text;
+
 // TODO COMPLETE
 public class RegisterFragment extends Fragment {
     private RegisterViewModel registerViewModel;
@@ -31,8 +36,16 @@ public class RegisterFragment extends Fragment {
 
         // TODO add register code... all text fields and onClick for "register" button
         final Button btn_submit_registration = (Button) root.findViewById(R.id.btn_submit_registration);
-        //btn_submit_registration.setOnClickListener(new View.OnClickListener(){
+        final TextView register_band_name = (TextView) root.findViewById(R.id.register_name);
+        //   final TextView register_password = (TextView) root.findViewById(R.id.);
 
+        btn_submit_registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View textView) {
+                String ArtistUsername = register_band_name.getText().toString();
+                //        String ArtistPassword = register_password.getText().toString();
+            }
+        });
 
 
         return root;
