6613b86e6391584356804721f2f41812b7e2b937
spncrf <spncrf@gmail.com>
2020-04-23 04:02:12 -0500

    updating listing tests

--- ProjectFiles/app/src/test/java/com/indiehood/app/MoreShowListingTests.java
+++ ProjectFiles/app/src/test/java/com/indiehood/app/MoreShowListingTests.java
@@ -12,14 +12,14 @@ package com.indiehood.app;
 public class MoreShowListingTests {
     @Test
     public void defaultConstructor_works() {
-            ShowListing testListing = new ShowListing(showID);
+            ShowListing testListing = new ShowListing();
             assertThat(testListing).isNotNull();
         }
 
     //Add a band named "testBand" and check that it was added correctly
     @Test
     public void addBandName_works() {
-        ShowListing testListing = new ShowListing(showID);
+        ShowListing testListing = new ShowListing();
         String showBand = "testBand";
         testListing.setBandName("testBand");
         assertThat(testListing.getBandName()).isEqualTo(showBand);
@@ -30,17 +30,17 @@ public class MoreShowListingTests {
     //This function should refer to the getStringifiedPrice and change the double value into "FREE"
     @Test
     public void getFreePrice(){
-        ShowListing testListing = new ShowListing(showID);
+        ShowListing testListing = new ShowListing();
         String freePrice = "0.0";
         testListing.setPrice(0.0);
-        assertThat(testListing.getStringifiedPrice()).isNotEqualTo(freePrice);
+        assertThat(testListing.priceFormatted).isNotEqualTo(freePrice);
     }
 
     //Show that if a setUserInterested is false, then the getUserInterested should not return True
     @Test
     public void addInterested_isFalse(){
         //should be false
-        ShowListing testListing = new ShowListing(showID);
+        ShowListing testListing = new ShowListing();
         testListing.setUserInterested(false);
         assertThat(testListing.getUserInterested()).isFalse();
     }
@@ -49,7 +49,7 @@ public class MoreShowListingTests {
     //show that getPrice should return a double and not an integer
     //Two cases -> one Equal to and one Not Equal to
     public void getPriceDouble_works(){
-        ShowListing testListing = new ShowListing(showID);
+        ShowListing testListing = new ShowListing();
         testListing.setPrice(10.0);
         Double price = 10.0;
         //Integer wrong = 10 ;
@@ -60,7 +60,7 @@ public class MoreShowListingTests {
     //Show that when a band name is "" then it is empty.
     @Test
     public void emptyBandName_works(){
-        ShowListing testListing = new ShowListing(showID);
+        ShowListing testListing = new ShowListing();
         String emptyName = "";
         testListing.setBandName(emptyName);
         assertThat(testListing.getBandName()).isEmpty();
--- ProjectFiles/app/src/test/java/com/indiehood/app/ShowListingTest.java
+++ ProjectFiles/app/src/test/java/com/indiehood/app/ShowListingTest.java
@@ -18,20 +18,20 @@ public class ShowListingTest {
 
     @Test
     public void defaultConstructor_notNull() {
-        ShowListing show = new ShowListing(showID);
+        ShowListing show = new ShowListing();
         assertNotNull(show);
     }
 
     @Test
     public void peopleInterested_1() {
-        ShowListing show = new ShowListing(showID);
+        ShowListing show = new ShowListing();
         show.setNumberInterested(1);
         assertEquals(show.getInterestedText(), "1 person is interested");
     }
 
     @Test
     public void peopleInterested_not1() {
-        ShowListing show = new ShowListing(showID);
+        ShowListing show = new ShowListing();
         int randomNumber = ThreadLocalRandom.current().nextInt(4) + 2;
         show.setNumberInterested(randomNumber);
         assertEquals(randomNumber + " people are interested", show.getInterestedText());
@@ -39,17 +39,17 @@ public class ShowListingTest {
 
     @Test
     public void showPrice_0() {
-        ShowListing show = new ShowListing(showID);
+        ShowListing show = new ShowListing();
         show.setPrice(0.0);
-        assertEquals(show.getStringifiedPrice(), "FREE");
+        assertEquals(show.priceFormatted, "FREE");
     }
 
     @Test
     public void showPrice_not0() {
-        ShowListing show = new ShowListing(showID);
+        ShowListing show = new ShowListing();
         int randomNumber = ThreadLocalRandom.current().nextInt(4) + 1;
         show.setPrice((double) randomNumber);
-        assertEquals("$" + randomNumber + ".00", show.getStringifiedPrice());
+        assertEquals("$" + randomNumber + ".00", show.priceFormatted);
     }
 
 
